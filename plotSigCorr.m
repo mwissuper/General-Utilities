a868bebd1f7d11882808d9126e56439807636028
M Wu <mwissuper@gmail.com>
2024-04-11 21:42:02 -0400

    kinectfor

--- plotSigCorr.m
+++ plotSigCorr.m
@@ -1,9 +1,9 @@
 function [r, p] = plotSigCorr(x,y,numrows,numcols,plotind)
 
 [p, r, test] = corr2vars(x,y);
-if p < 0.05
+% if p < 0.05
     subplot(numrows,numcols,plotind)
     c = polyfit(x,y,1);
     plot(x,polyval(c,x),'k-'); 
-end
+% end
 tname = sprintf('p: %.2f, r: %.3f',p,r); title(tname);
